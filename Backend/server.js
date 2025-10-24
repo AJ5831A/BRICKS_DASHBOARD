ca83a2ed6908d7688a6ffa71bc01f21c8013d470
AJ5831A <jakhararyan27@gmail.com>
2025-10-24 23:34:05 +0530

    Assignments added

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 connectToDB();
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(express.json());
